ade51f9744d23025d3be7f6cf0c999c353aadd63
Jordan2551 <Jordan1558@gmail.com>
2020-06-05 18:17:33 -0400

    Updates

---
+++ precache-manifest.f84f596af7714bde7029803cd97eb4d7.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32ed69ac0edc66c5215da31d283b6f1e",
+    "url": "/speak-to-a-lawyer/index.html"
+  },
+  {
+    "revision": "47efc16c1a2f0e48de35",
+    "url": "/speak-to-a-lawyer/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "9adcda27bfbcd0977718",
+    "url": "/speak-to-a-lawyer/static/css/main.26b282bf.chunk.css"
+  },
+  {
+    "revision": "47efc16c1a2f0e48de35",
+    "url": "/speak-to-a-lawyer/static/js/2.6407a8c0.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/speak-to-a-lawyer/static/js/2.6407a8c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9adcda27bfbcd0977718",
+    "url": "/speak-to-a-lawyer/static/js/main.03bfe22b.chunk.js"
+  },
+  {
+    "revision": "b4839d5c47c063b39c64",
+    "url": "/speak-to-a-lawyer/static/js/runtime-main.730e2af9.js"
+  },
+  {
+    "revision": "5096b1ad6e35cff85809702d388c0694",
+    "url": "/speak-to-a-lawyer/static/media/cards.5096b1ad.png"
+  },
+  {
+    "revision": "61b204985964a51f053af31b2252b2b4",
+    "url": "/speak-to-a-lawyer/static/media/hero-bg.61b20498.jpg"
+  },
+  {
+    "revision": "1ac3d7c13e2533be7be0925b0c25ab13",
+    "url": "/speak-to-a-lawyer/static/media/logo.1ac3d7c1.png"
+  },
+  {
+    "revision": "3aae38daf260a118423b511a89198e66",
+    "url": "/speak-to-a-lawyer/static/media/shield.3aae38da.png"
+  },
+  {
+    "revision": "22f68b96d555159930163b87369b66ea",
+    "url": "/speak-to-a-lawyer/static/media/stripe-logo.22f68b96.png"
+  }
+]);
